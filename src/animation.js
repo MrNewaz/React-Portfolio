35a6b6bf0542fd5a00122e9cbd3050e54d6d5032
MrNewaz <saifur.saif@northsouth.edu>
2021-02-08 14:00:53 +0600

    part 14

--- src/animation.js
+++ src/animation.js
@@ -8,13 +8,57 @@ export const pageAnimation = {
     y: 0,
     transition: {
       duration: 1,
+      when: "beforeChildren",
+      staggerChildren: 0.25,
     },
   },
   exit: {
     opacity: 0,
     y: 300,
     transition: {
-      duration: 1,
+      duration: 0.5,
+    },
+  },
+};
+
+export const titleAnim = {
+  hidden: {
+    y: 200,
+  },
+  show: {
+    y: 0,
+    transition: {
+      duration: 0.75,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const fade = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+
+    transition: {
+      duration: 0.75,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const photoAnim = {
+  hidden: {
+    opacity: 0,
+    scale: 1.5,
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.75,
+      ease: "easeOut",
     },
   },
 };
